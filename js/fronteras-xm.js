a0b3bcc1ead3872a1672942fc42aff1d8ffd5e7d
Francisco Lopez <franclopez@gmail.com>
2013-11-20 17:14:56 -0500

    Ajustes varios

--- js/fronteras-xm.js
+++ js/fronteras-xm.js
@@ -16,9 +16,6 @@ $(function() {
                loginError.text('Problemas conectando con el backend. Por favor intente mas tarde');
             });
             
-            
-            
-        
             /************ login ************/
             var loginForm = $('#login_form');
             var loginError =  $('#login_error');
@@ -26,44 +23,46 @@ $(function() {
             loginForm.on('submit', function(e) {
                 e.preventDefault();
                 loginError.text('');
-                
-                //flood control
-                if(loginForm.hasClass('loading')) {
-                    return false;
-                }
-                loginForm.addClass('loading');
-                
+                $.mobile.loading('show');
                 var usernameField = loginForm.find('[name="username"]');
                 var username = $.trim(usernameField.val());
                 var passwordField = loginForm.find('[name="password"]');
                 var password = $.trim(passwordField.val());
+    			console.log("Autenticando usuario...");
                 console.log(username);
                 console.log(password);
                 var user = Kinvey.getActiveUser();
-                if (null !== user){
-                    $.mobile.changePage('#menu'); //change to menu page
-                }
-    			else {
-					Kinvey.User.login(username, password, {
+                
+				Kinvey.User.logout({
 						success: function() {
+							console.log("Desconectando");
+						},
+						error: function(e) {
+							console.log("Usuario no conectado");
+						}
+				});
+				
+				Kinvey.User.login(username, password, {
+						success: function() {
+						console.log("Loggeado.");
 							loginForm.removeClass('loading');
+							$.mobile.loading('hide');
 							usernameField.val(''); //clear fields
 							passwordField.val('');
 							$.mobile.changePage('#menu'); //change to menu page
 						},
 						error: function(error){
 							console.log(error);
-							loginForm.removeClass('loading');
-							loginError.text('Por Favor ingrese un usuario y contraseña validos');
+							$.mobile.loading('hide');
+							loginError.text('Por Favor Ingrese Un Usuario y Contraseña Válidos');
 						}
-					});
-				}
-                return false;
+				 });
+				 return false;
             });
             
             //Logout
-             var button = document.getElementById('logout');
-             button.addEventListener('click', function() {
+			
+			$("#logout").click(function () {
                 console.log("Logging out");
                 var user = Kinvey.getActiveUser();
                 if (null !== user){
@@ -79,7 +78,7 @@ $(function() {
             });
 			
 			//Menu Page
-			$(document).on("pageinit", "#menu", function () {
+			$(document).on("pageshow", "#menu", function () {
 				var user = Kinvey.getActiveUser();
 				console.log(user);
 				var txtBienv = "Bienvenido: " + user.first_name + " " + user.last_name;
@@ -89,9 +88,10 @@ $(function() {
 			
 			//Busqueda de requerimientos
 			$(document).on("pageinit", "#requerimientosBusqueda", function () {
+				console.log("pageinit del requerimientosBusqueda");
 				var promise = Kinvey.DataStore.find('EstadosRequerimiento', null, {
 					success: function(items) {
-					   var list = $("#RevStatus");
+					   var list = $("#reqBusEstReq");
 					   $.each(items, function(index, item) {
 						  list.append(new Option(item.Nombre, item.Id));
 					   });
@@ -99,19 +99,24 @@ $(function() {
 				  });
                   var promise2 = Kinvey.DataStore.find('TiposRequerimiento', null, {
     				success: function(items) {
-					   var list = $("#ReqType");
+					   var list = $("#reqBusTipReq");
 					   $.each(items, function(index, item) {
 						  list.append(new Option(item.Nombre, item.Id));
 					   });
 					}
 				  });
             });
-            
+			
+			$(document).on("pageshow", "#requerimientosBusqueda", function () {
+					console.log("pageshow del requerimientosBusqueda");
+			});
+			
+			            
             //Busqueda de Fronteras
     		$(document).on("pageinit", "#fronterasBusqueda", function () {
 				var promise = Kinvey.DataStore.find('TiposFrontera', null, {
 					success: function(items) {
-					   var list = $("#FronteraType");
+					   var list = $("#fronBusFronTipo");
 					   $.each(items, function(index, item) {
 						  list.append(new Option(item.Nombre, item.Id));
 					   });
@@ -119,7 +124,7 @@ $(function() {
 				});
 				var promise2 = Kinvey.DataStore.find('NivelesTension', null, {
     				success: function(items) {
-					   var list = $("#nivelTension");
+					   var list = $("#fronBusNivTension");
 					   $.each(items, function(index, item) {
 						  list.append(new Option(item.Descripcion, item.Id));
 					   });
@@ -129,7 +134,7 @@ $(function() {
                 queryDpto.ascending('NombreDepartamento');
                 var promiseDepto = Kinvey.DataStore.find('Departamentos', queryDpto, {
     				success: function(items) {
-					   var list = $("#departamento");
+					   var list = $("#fronBusDepartamento");
 					   $.each(items, function(index, item) {
 						  list.append(new Option(item.NombreDepartamento, item.IdDepartamento));
 					   });
@@ -137,22 +142,40 @@ $(function() {
 				});
 			});
             
-            $('#departamento').change(function() {
+            $('#fronBusDepartamento').change(function() {
+			  $.mobile.loading('show');
 			  var query = new Kinvey.Query();
               query.ascending('NombreCiudad');
 			  query.equalTo('IdDepartamento', this.value);
-              $("#ciudad").empty();
-              $("#ciudad").append(new Option("Seleccione...", ''));
-              $("#ciudad").selectmenu('refresh');
-              $("#ciudad").prop('selectedIndex', 0);
+              $("#fronBusCiudad").empty();
+              $("#fronBusCiudad").append(new Option("Seleccione...", ''));
+              $("#fronBusCiudad").selectmenu('refresh');              
+			  $("#fronBusCiudad").prop('selectedIndex', 0);
               var promiseCiudad = Kinvey.DataStore.find('Ciudades', query, {
     				success: function(items) {
-					   var list = $("#ciudad");
+					   var list = $("#fronBusCiudad");
 					   $.each(items, function(index, item) {
 						  list.append(new Option(item.NombreCiudad, item.IdCiudad));
 					   });
+					   $.mobile.loading('hide');
 					}
 				});
 			});
+			
+			//Realizar Busqueda de Requerimientos
+            $("#reqBusSearch").click(function () {
+                 $.mobile.loading('show');
+                 console.log("Buscando requerimientos....");
+                 $.mobile.changePage('#detalleRequerimiento');
+                 $.mobile.loading('hide');
+            });
             
+            
+            //Realizar Busqueda de Fronteras
+            $("#fronBusSearch").click(function () {
+                 $.mobile.loading('show');
+                 console.log("Buscando fronteras....");
+                 $.mobile.changePage('#detalleFronteras');
+                 $.mobile.loading('hide');
+            });
 });		
